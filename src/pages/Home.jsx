a21ed11b8fb03e9e33fa0a438ff822e8e9e23945
Aidar <aidar1910aaad@gmail.com>
2022-06-12 03:09:03 +0600

    first com

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Info from "../components/Info";
+
+const Home = () => {
+  return(
+      <div>
+          <Navbar/>
+          <Info/>
+      </div>
+  )
+}
+
+export default Home
