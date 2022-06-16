ad602b944384a903e75ffdfd0b9e754a14de6547
Aidar <aidar1910aaad@gmail.com>
2022-06-16 15:22:52 +0600

    a

---
+++ src/Data/RestData.js
@@ -0,0 +1,74 @@
+export const Restaurants = [
+    {
+        img: "/assets/img.png",
+        title: "MeatPoint",
+        star: 4.8,
+        location: "Uly Dala Avenue 11/3",
+        shed: "Круглосуточно",
+        average: 4000,
+        cat: ["Халал", "Халал", "Халал"]
+    },
+    {
+        img: "",
+        title: "MeatPoint",
+        star: 4.5,
+        location: "Uly Dala Avenue 11/3",
+        shed: "Круглосуточно",
+        average: 4000,
+        cat: ["Халал", "Халал", "Халал"]
+    },
+    {
+        img: "",
+        title: "MeatPoint",
+        star: 4.5,
+        location: "Uly Dala Avenue 11/3",
+        shed: "Круглосуточно",
+        average: 4000,
+        cat: ["Халал", "Халал", "Халал"]
+    },
+    {
+        img: "",
+        title: "MeatPoint",
+        star: 4.5,
+        location: "Uly Dala Avenue 11/3",
+        shed: "Круглосуточно",
+        average: 4000,
+        cat: ["Халал", "Халал", "Халал"]
+    },
+    {
+        img: "",
+        title: "MeatPoint",
+        star: 4.5,
+        location: "Uly Dala Avenue 11/3",
+        shed: "Круглосуточно",
+        average: 4000,
+        cat: ["Халал", "Халал", "Халал"]
+    },
+    {
+        img: "",
+        title: "MeatPoint",
+        star: 4.5,
+        location: "Uly Dala Avenue 11/3",
+        shed: "Круглосуточно",
+        average: 4000,
+        cat: ["Халал", "Халал", "Халал"]
+    },
+    {
+        img: "",
+        title: "MeatPoint",
+        star: 4.5,
+        location: "Uly Dala Avenue 11/3",
+        shed: "Круглосуточно",
+        average: 4000,
+        cat: ["Халал", "Халал", "Халал"]
+    },
+    {
+        img: "",
+        title: "MeatPoint",
+        star: 4.5,
+        location: "Uly Dala Avenue 11/3",
+        shed: "Круглосуточно",
+        average: 4000,
+        cat: ["Халал", "Халал", "Халал"]
+    },
+]
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Info from "../components/Info";
+import Rest from "../components/Rest";
+
+const Home = () => {
+  return(
+      <div>
+          <Navbar/>
+          <Info/>
+          <Rest/>
+      </div>
+  )
+}
+
+export default Home
